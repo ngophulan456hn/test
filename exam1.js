95a753455631032d8f4bd741103a5d6442755e56
Phu Lan <ngophulan456hn@gmail.com>
2025-04-21 13:28:47 +0700

    Add new playground.js file for experimentation and testing

--- exam1.js
+++ exam1.js
@@ -1,32 +1,33 @@
-var arr = ['a', 'ab', 'abc', 'cd', 'def', 'gh'];
+var arr = ["a", "ab", "abc", "cd", "def", "gh"];
 
 function stringsLengthThatAppear(arr) {
-    if (Array.isArray(arr)) {
-        var appear = {};
-        arr.forEach(i => {
-            var exists = i.toString().length in appear;
-            if (exists) {
-                appear[i.toString().length] += 1;
-            } else {
-                appear[i.toString().length] = 1;
-            }
-        })
-        var max = 0;
-        for (var key in appear) {
-            if (appear[key] > max) max = appear[key];
-        }
+  if (Array.isArray(arr)) {
+    var appear = {};
+    arr.forEach((i) => {
+      var exists = i.toString().length in appear;
+      if (exists) {
+        appear[i.toString().length] += 1;
+      } else {
+        appear[i.toString().length] = 1;
+      }
+    });
+    var max = 0;
+    for (var key in appear) {
+      if (appear[key] > max) max = appear[key];
+    }
 
-        return arr.filter(i => i.toString().length == Object.keys(appear).find(key => appear[key] === max));
-    } 
-    return "Not a array";
+    return arr.filter(
+      (i) =>
+        i.toString().length ==
+        Object.keys(appear).find((key) => appear[key] === max)
+    );
+  }
+  return "Not a array";
 }
 
 console.log(stringsLengthThatAppear(arr));
 
-//test-case 
+//test-case
 var test_case1 = 1;
-var test_case2 = [1,12,23,34,456,789];
+var test_case2 = [1, 12, 23, 34, 456, 789];
 var test_case3 = null;
-
-
-
--- exam2.js
+++ exam2.js
@@ -1,23 +1,23 @@
 var arr = [1, 4, 2, 3, 5];
 
 function sumOfTop2(arr) {
-    if (Array.isArray(arr)) {
-        if (arr.length < 2) return "Array size must be more than 1";
-        var max = arr[0];
-        var secondMax = arr[0];
-        var check = false
-        arr.forEach(i => {
-            if (typeof (i) != 'number') check = true;
-            if (i > max) {
-                secondMax = max;
-                max = i;
-            }
-        })
-        if (check) return "Array must contain only number";
-        if (max == secondMax) return "Array dont have second max";
-        return max + secondMax;
-    }
-    return "Not a array";
+  if (Array.isArray(arr)) {
+    if (arr.length < 2) return "Array size must be more than 1";
+    var max = arr[0];
+    var secondMax = arr[0];
+    var check = false;
+    arr.forEach((i) => {
+      if (typeof i != "number") check = true;
+      if (i > max) {
+        secondMax = max;
+        max = i;
+      }
+    });
+    if (check) return "Array must contain only number";
+    if (max == secondMax) return "Array dont have second max";
+    return max + secondMax;
+  }
+  return "Not a array";
 }
 
 console.log(sumOfTop2(arr));
